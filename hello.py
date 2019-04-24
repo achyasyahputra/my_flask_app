38cfec823221f8ba79345c93275e1f22c551bbaa
Ubuntu <ubuntu@outgle-101.c.kurio-dev.internal>
2019-04-24 07:42:45 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/') 
+def hello():
+    if True:
+        return 'Hello World, This is Kurio!', 200
+    return "Can't find Kurio", 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
